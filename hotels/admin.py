76df8d39dea390e13395e3edb0aa348eab9b18c1
levi <levidasilva@live.com>
2024-10-09 19:45:03 -0300

    Removendo diretórios duplicados

--- hotels/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Hotel, Quarto, Reserva
-
-admin.site.register(Hotel)
-admin.site.register(Quarto)
-admin.site.register(Reserva)
--- hotels/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Reserva
-
-
-class ReservaForm(forms.ModelForm):
-    class Meta:
-        model = Reserva
-        fields = ['check_in', 'check_out']
